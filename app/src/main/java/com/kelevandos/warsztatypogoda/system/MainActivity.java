c611703c0f806cf52d27bfc2f4ddf725e703e329
Kelevandos <kelevandos@gmail.com>
2017-10-08 17:34:12 +0200

    Add UI

--- app/src/main/java/com/kelevandos/warsztatypogoda/system/MainActivity.java
+++ app/src/main/java/com/kelevandos/warsztatypogoda/system/MainActivity.java
@@ -1,22 +1,30 @@
-package com.kelevandos.warsztatypogoda;
+package com.kelevandos.warsztatypogoda.system;
 
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.kelevandos.warsztatypogoda.Pogoda;
+import com.kelevandos.warsztatypogoda.R;
 import com.kelevandos.warsztatypogoda.network.WeatherListener;
 import com.kelevandos.warsztatypogoda.network.WeatherProvider;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity implements WeatherListener {
+public class MainActivity extends AppCompatActivity implements WeatherListener, View.OnClickListener {
 
     private TextView miasto;
     private TextView temperatura;
     private ImageView ikona;
     private TextView opis;
+
+    private EditText nazwaMiasta;
+    private Button pobierz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,11 @@ public class MainActivity extends AppCompatActivity implements WeatherListener {
         ikona = (ImageView) findViewById(R.id.ikona);
         opis = (TextView) findViewById(R.id.opis);
         opis.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        nazwaMiasta = (EditText) findViewById(R.id.nazwaMiasta);
+        pobierz = (Button) findViewById(R.id.pobierz);
+
+        pobierz.setOnClickListener(this);
     }
 
     @Override
@@ -54,6 +67,12 @@ public class MainActivity extends AppCompatActivity implements WeatherListener {
     @Override
     public void onFailure(Throwable t) {
         Toast.makeText(this, "Coś poszło nie tak", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        WeatherProvider provider = new WeatherProvider(this);
+        provider.getWeather(nazwaMiasta.getText().toString());
     }
 
     private String getIconUrl(String id) {
